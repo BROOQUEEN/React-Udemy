bea382789d039e019e707906e8238a774715a5e3
brooqueen <dimabrooqueen@gmail.com>
2024-08-13 20:59:46 +0300

    42. Добавление Двустороннего Связывания

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -36,21 +36,26 @@ const CostForm = () => {
         };
 
         console.log(costData);
+
+        // очистка инпутов после клика на кнопку
+        setInputName('');
+        setInputAmount('');
+        setInputDate('');
     };
 
     return (
         <form className="new-cost__controls" onSubmit={submitHandler}>
             <div className="new-cost__control">
                 <label>Название</label>
-                <input type="text" onChange={nameChangeHandler} />
+                <input type="text" value={inputName} onChange={nameChangeHandler} />
             </div>
             <div className="new-cost__control">
                 <label>Сумма</label>
-                <input type="number" onChange={amountChangeHandler} min='0.01' step='0.01' />
+                <input type="number" value={inputAmount} onChange={amountChangeHandler} min='0.01' step='0.01' />
             </div>
             <div className="new-cost__control">
                 <label>Дата</label>
-                <input type="date" onChange={dateChangeHandler} min='2019-01-01' step='2022-12-31' />
+                <input type="date" value={inputDate} onChange={dateChangeHandler} min='2019-01-01' step='2022-12-31' />
             </div>
             <div className="new-cost__actions">
                 <button type="submit">Добавить расход</button>
