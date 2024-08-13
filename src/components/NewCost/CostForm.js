6ca1928c41f94511e9a628ee02800675421bb75d
brooqueen <dimabrooqueen@gmail.com>
2024-08-13 20:36:59 +0300

    40. Обновление Состояния, Зависящего от Предыдущего Состояния

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -3,38 +3,27 @@ import './CostForm.css';
 
 const CostForm = () => {
 
-    // const [name, setName] = useState('');
-    // const [amount, setAmount] = useState('');
-    // const [date, setDate] = useState('');
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
 
-    const [userInput, setUserInput] = useState({
-        name: '',
-        amount: '',
-        date: ''
-    });
+    // const [userInput, setUserInput] = useState({
+    //     name: '',
+    //     amount: '',
+    //     date: ''
+    // });
 
     const nameChangeHandler = (event) => {
-        // setName(event.target.value);
-        setUserInput({
-            ...userInput,
-            name: event.target.value
-        })
+        setName(event.target.value);
+        
     };
 
     const amountChangeHandler = (event) => {
-        // setAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            amount: event.target.value
-        })
+        setAmount(event.target.value);
     };
     
     const dateChangeHandler = (event) => {
-        // setDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            date: event.target.value
-        })
+        setDate(event.target.value);
     };
 
     return <form className="new-cost__controls">
