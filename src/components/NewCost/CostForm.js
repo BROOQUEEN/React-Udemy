519acd56f33dc66e1ff61b4ed919586f1d983342
brooqueen <dimabrooqueen@gmail.com>
2024-08-13 20:52:36 +0300

    41. Обработка Отправки Формы

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -3,9 +3,9 @@ import './CostForm.css';
 
 const CostForm = () => {
 
-    const [name, setName] = useState('');
-    const [amount, setAmount] = useState('');
-    const [date, setDate] = useState('');
+    const [inputName, setInputName] = useState('');
+    const [inputAmount, setInputAmount] = useState('');
+    const [inputDate, setInputDate] = useState('');
 
     // const [userInput, setUserInput] = useState({
     //     name: '',
@@ -14,35 +14,49 @@ const CostForm = () => {
     // });
 
     const nameChangeHandler = (event) => {
-        setName(event.target.value);
+        setInputName(event.target.value);
         
     };
 
     const amountChangeHandler = (event) => {
-        setAmount(event.target.value);
+        setInputAmount(event.target.value);
     };
     
     const dateChangeHandler = (event) => {
-        setDate(event.target.value);
+        setInputDate(event.target.value);
     };
 
-    return <form className="new-cost__controls">
-        <div className="new-cost__control">
-            <label>Название</label>
-            <input type="text" onChange={nameChangeHandler} />
-        </div>
-        <div className="new-cost__control">
-            <label>Сумма</label>
-            <input type="number" min='0.01' step='0.01' />
-        </div>
-        <div className="new-cost__control">
-            <label>Дата</label>
-            <input type="date" min='2019-01-01' step='2022-12-31' />
-        </div>
-        <div className="new-cost__actions">
-            <button type="submit">Добавить расход</button>
-        </div>
-    </form>
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const costData = {
+            name: inputName,
+            amount: inputAmount,
+            date: new Date(inputDate)
+        };
+
+        console.log(costData);
+    };
+
+    return (
+        <form className="new-cost__controls" onSubmit={submitHandler}>
+            <div className="new-cost__control">
+                <label>Название</label>
+                <input type="text" onChange={nameChangeHandler} />
+            </div>
+            <div className="new-cost__control">
+                <label>Сумма</label>
+                <input type="number" onChange={amountChangeHandler} min='0.01' step='0.01' />
+            </div>
+            <div className="new-cost__control">
+                <label>Дата</label>
+                <input type="date" onChange={dateChangeHandler} min='2019-01-01' step='2022-12-31' />
+            </div>
+            <div className="new-cost__actions">
+                <button type="submit">Добавить расход</button>
+            </div>
+        </form>
+    )
 }
 
 export default CostForm;
